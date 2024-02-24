a3bf2e57df1b2870dc67867e8880876112a7bc23
Akhil Das <akhilpic1@gmail.com>
2024-02-24 16:42:49 +0530

    linked List

---
+++ linkedlist/link.go
@@ -0,0 +1,64 @@
+package Linklist
+
+import "fmt"
+
+type Node struct {
+	Data int
+	Next *Node
+}
+type Linked struct {
+	Head   *Node
+	Lenght int
+}
+
+func (l *Linked) Prepend(n *Node) {
+
+	second := l.Head
+
+	l.Head = n
+	l.Head.Next = second
+	l.Lenght++
+}
+
+//Print the linkedList  data
+
+func (l Linked) PrinDataOnNode() {
+
+	toPrint := l.Head
+	for l.Lenght != 0 {
+		fmt.Printf("%d ", toPrint.Data )
+		toPrint = toPrint.Next
+		l.Lenght--
+
+	}
+	fmt.Printf("\n")
+}
+
+//deleting the value from the linked List
+
+func (l *Linked) DeleteLinkedListData(value int) {
+
+	previousToDelete := l.Head
+
+	if l.Head == nil {
+		return
+
+	}
+	if l.Head.Data == value {
+		l.Head = l.Head.Next
+		l.Lenght--
+		return
+	}
+
+	for previousToDelete.Next.Data != value {
+
+		if previousToDelete.Next.Next == nil {
+			fmt.Println("there is no data for delete")
+			return
+		}
+		previousToDelete = previousToDelete.Next
+	}
+
+	previousToDelete.Next = previousToDelete.Next.Next
+	l.Lenght--
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	Linklist "stack/linkedlist"
+)
 
 // Stack
 type Stack struct {
@@ -18,19 +20,60 @@ func (s *Stack) Push(i int) {
 // return the removed value
 
 func (s *Stack) Pop() int {
-	 l := len(s.items)-1
+	l := len(s.items) - 1
 	removedElement := s.items[l]
 	s.items = s.items[:l]
 	return removedElement
 }
 func main() {
 
-	mystack := Stack{}
-	fmt.Println(mystack)
-	mystack.Push(100)
-	mystack.Push(200)
-	mystack.Push(300)
-	fmt.Println(mystack)
-	mystack.Pop()
-	fmt.Println(mystack)
+	//stack
+
+	// mystack := Stack{}
+	// fmt.Println(mystack)
+	// mystack.Push(100)
+	// mystack.Push(200)
+	// mystack.Push(300)
+	// fmt.Println(mystack)
+	// mystack.Pop()
+	// fmt.Println(mystack)
+
+	//queue and deque
+	// q := Queue.Queue{}
+
+	// q.Queue(2)
+	// q.Queue(20)
+	// q.Queue(40)
+	// fmt.Println(q)
+
+	// Linked list
+
+	node1 := &Linklist.Node{Data: 10}
+	node2 := &Linklist.Node{Data: 20}
+	node3 := &Linklist.Node{Data: 30}
+	node4 := &Linklist.Node{Data: 40}
+	node5 := &Linklist.Node{Data: 50}
+	node6 := &Linklist.Node{Data: 60}
+	node7 := &Linklist.Node{Data: 70}
+	linkedList := Linklist.Linked{}
+
+	linkedList.Prepend(node1)
+	linkedList.Prepend(node2)
+	linkedList.Prepend(node3)
+	linkedList.Prepend(node4)
+	linkedList.Prepend(node5)
+	linkedList.Prepend(node6)
+	linkedList.Prepend(node7)
+
+	linkedList.PrinDataOnNode()
+
+	// fmt.Println(linkedList)
+
+	linkedList.DeleteLinkedListData(70)
+	linkedList.PrinDataOnNode()
+	// fmt.Println("LinkedList after delete",linkedList)
+
+	// fmt.Println("Length of linked list:", linkedList.Lenght)
+	// fmt.Println("Data of the first node:", linkedList.Head.Data)
+
 }
